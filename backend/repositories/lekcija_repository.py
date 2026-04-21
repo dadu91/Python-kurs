3145adda9d2c198a117cc6c303d4cc36b8792f1c
dadu91 <vladimir.adzic5@gmail.com>
2026-04-21 03:58:25 +0200

    Revert "backend gotov"
    This reverts commit 9c3f10b285933fd7fbcca31b6bf6c2ddff8bd472.

--- backend/repositories/lekcija_repository.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from backend.models.lekcija_model import Lekcija
-
-
-def get_all_lekcije(db: Session):
-    return db.query(Lekcija).all()
--- backend/services/lekcija_service.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from backend.repositories.lekcija_repository import get_all_lekcije
-
-
-def procitaj_sve_lekcije(db: Session):
-    return get_all_lekcije(db)
