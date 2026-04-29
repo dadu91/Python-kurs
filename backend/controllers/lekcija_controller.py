acee54e78796f454eb0c415e49755505d3307dd6
peshaa2 <npetar076@gmail.com>
2026-04-29 13:41:23 +0200

    Zavrsen CRUD za lekcije

--- backend/controllers/lekcija_controller.py
+++ backend/controllers/lekcija_controller.py
@@ -1,13 +1,39 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from database import get_db
-from schemas.lekcija_schema import LekcijaOut
-from services.lekcija_service import procitaj_sve_lekcije
+from schemas.lekcija_schema import LekcijaOut, LekcijaCreate, LekcijaUpdate
+from services import lekcija_service
 
 router = APIRouter(prefix="/lekcije", tags=["Lekcije"])
 
 
 @router.get("/", response_model=list[LekcijaOut])
 def vrati_lekcije(db: Session = Depends(get_db)):
-    return procitaj_sve_lekcije(db)
+    return lekcija_service.procitaj_sve_lekcije(db)
+
+
+@router.get("/{lekcija_id}", response_model=LekcijaOut)
+def vrati_lekciju_po_id(lekcija_id: int, db: Session = Depends(get_db)):
+    return lekcija_service.get_lekcija_id(db, lekcija_id)
+
+
+@router.post("/", response_model=LekcijaOut, status_code=status.HTTP_201_CREATED)
+def dodaj_lekciju(lekcija: LekcijaCreate, db: Session = Depends(get_db)):
+    return lekcija_service.create_lekcija(db, lekcija)
+
+
+@router.put("/{lekcija_id}", response_model=LekcijaOut)
+def izmijeni_lekciju(
+    lekcija_id: int,
+    lekcija_update: LekcijaUpdate,
+    db: Session = Depends(get_db)
+):
+    lekcija = lekcija_service.get_lekcija_id(db, lekcija_id)
+    return lekcija_service.update_lekcija(db, lekcija, lekcija_update)
+
+
+@router.delete("/{lekcija_id}", status_code=status.HTTP_204_NO_CONTENT)
+def obrisi_lekciju(lekcija_id: int, db: Session = Depends(get_db)):
+    lekcija = lekcija_service.get_lekcija_id(db, lekcija_id)
+    lekcija_service.delete_lekcija(db, lekcija)
--- backend/repositories/lekcija_repository.py
+++ backend/repositories/lekcija_repository.py
@@ -4,3 +4,29 @@ from models.lekcija_model import Lekcija
 
 def get_all_lekcije(db: Session):
     return db.query(Lekcija).all()
+
+
+def get_lekcija_by_id(db: Session, lekcija_id: int):
+    return db.query(Lekcija).filter(Lekcija.id == lekcija_id).first()
+
+
+def get_lekcija_by_naziv(db: Session, naziv: str):
+    return db.query(Lekcija).filter(Lekcija.naziv == naziv).first()
+
+
+def create_lekcija(db: Session, lekcija: Lekcija):
+    db.add(lekcija)
+    db.commit()
+    db.refresh(lekcija)
+    return lekcija
+
+
+def update_lekcija(db: Session, lekcija: Lekcija):
+    db.commit()
+    db.refresh(lekcija)
+    return lekcija
+
+
+def delete_lekcija(db: Session, lekcija: Lekcija):
+    db.delete(lekcija)
+    db.commit()
